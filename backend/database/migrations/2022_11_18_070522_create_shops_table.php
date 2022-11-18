a837067e3a5bd352d377f9e3f366d94cd22fddac
IqBatz <78571493+IqBatz@users.noreply.github.com>
2022-11-18 02:03:13 -0600

    mas migracioes

---
+++ backend/database/migrations/2022_11_18_070522_create_shops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('Nombre');
+            $table->string('RUC');
+            $table->string('Direccion');
+            $table->string('Telefono');
+            $table->bit('Nombre');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+};
---
+++ backend/database/migrations/2022_11_18_074511_create_proveedores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proveedores', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('Nombre');
+            $table->string('Direccion');
+            $table->string('Telefono');
+            $table->string('email');
+            $table->bit('Activo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proveedores');
+    }
+};
