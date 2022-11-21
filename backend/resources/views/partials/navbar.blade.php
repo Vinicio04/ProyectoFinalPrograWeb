13bd6462d2e83f4b2c21e65fb205cdf2d425211f
Vinicio04 <99298787+Vinicio04@users.noreply.github.com>
2022-11-21 04:32:54 -0600

    navbar

--- backend/resources/views/partials/navbar.blade.php
+++ backend/resources/views/partials/navbar.blade.php
@@ -1,16 +1,14 @@
 <nav class="navbar navbar-expand-md navbar-dark  bg-dark shadow-sm">
     <div class="container">
         <a class="navbar-brand " href="{{ url('/') }}">
-           Tienda de Productos electronicos TecnoFacil
+           Tienda TecnoFacil
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('shop') }}">TIENDA</a>
-                </li>
+                
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('clientes.index') }}">Clientes</a>
                 </li>
@@ -20,6 +18,14 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('proveedores.index') }}">Proveedores   </a>
                 </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('products.index') }}">Productos   </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('shops.index') }}">Sucursales   </a>
+                </li>
+
 
 
 
@@ -42,7 +48,7 @@
                     </li>
     
 
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="width: 450px; padding: 0px; border-color: #0893ef">
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="width: 450px; padding: 0px; border-color: #0f8bdd">
                         <ul class="list-group" style="margin: 20px;">
                             @include('partials.cart-drop')
                         </ul>
