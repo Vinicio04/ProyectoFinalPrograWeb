5ae406914c606dac684e20609c08ea70ef7290fa
IqBatz <78571493+IqBatz@users.noreply.github.com>
2022-11-20 20:04:24 -0600

    intentomapa

--- backend/resources/views/shop/form.blade.php
+++ backend/resources/views/shop/form.blade.php
@@ -27,8 +27,57 @@
             {!! $errors->first('Activo', '<div class="invalid-feedback">:message</div>') !!}
         </div>
 
+
+
     </div>
+
+    <div class="mapafrm" id="mapa">
+
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="mapafrm" id="mapa">
+
+            </div>
+        </div>
+    </div>
+
     <div class="box-footer mt20">
         <button type="submit" class="btn btn-primary">Submit</button>
     </div>
+
 </div>
+
+<script>
+
+    function iniciarMapa(){
+        var latitud = 14.8426384;
+        var longitud = -91.5276544;
+
+        coordenadas={
+            lng: longitud,
+            lat: latitud
+        }
+
+        generarMapa(coordenadas);
+    }
+    function generarMapa(coordenadas){
+        var mapa = new google.maps.Map(document.getElementById('mapa'),{
+            zoom:15,
+            center: new google.maps.LatLng(coordenadas.lat, coordenadas.lng)
+        });
+
+        marcador = new google.maps.Marker({
+            map: mapa,
+            draggable: true,
+            position: new google.maps.LatLng(coordenadas.lat, coordenadas.lng)
+        });
+
+        marcador.addListener('dragend', function(event){
+document.getElementById('Direccion').value = this.getPosition();
+        });
+    }
+</script>
+
+<script src="https://maps.google.com/maps/api/js?key=&callback=iniciarMapa%22%3E"></script>
